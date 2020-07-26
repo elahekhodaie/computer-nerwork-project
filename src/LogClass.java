7fc5f24c358bca8f1f832679bfe812a1787c260e
ellykhodaie <elahekhodaie@yahoo.com>
2020-07-26 16:33:32 +0430

    the log class was added

--- src/LogClass.java
+++ src/LogClass.java
@@ -15,5 +15,16 @@ public class LogClass {
         System.out.println("[" + utc_datetime + "] " + request_line + " " + response_status_code );
     }
 
+    public void print_error_message(StatusCode statusCode){
+        if (statusCode == StatusCode.BAD_REQUEST_400)
+            System.out.println("Simple HTML message describing that the request is not accepted !");
+        else if (statusCode == StatusCode.METHOD_NOT_ALLOWED_405)
+            System.out.println("Simple HTML message describing that the method is not supported !");
+        else if (statusCode == StatusCode.NOT_IMPLEMENTED_501)
+            System.out.println("Simple HTML message describing that the method is not recognized !");
+        else if (statusCode == StatusCode.NOT_FOUND_404)
+            System.out.println("HTML message describing a file was not found !");
+    }
+
 
 }
